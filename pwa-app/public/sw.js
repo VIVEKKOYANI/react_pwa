745b82a49e64205899a01fdb2d63d9b6c4f8fa71
vivek koyani <vivek.koyani@gmail.com>
2024-06-16 19:14:00 +0530

    add offline mode

--- pwa-app/public/sw.js
+++ pwa-app/public/sw.js
@@ -1,1 +1,37 @@
-console.log("ws file is public folder");
+const cacheName = 'appV1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll([
+                '/static/js/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js',
+                '/manifest.json',
+                '/index.html',
+                '/'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating...');
+    // Optionally, you can remove old caches here if necessary
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).then((fetchResponse) => {
+                return caches.open(cacheName).then((cache) => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        }).catch(() => {
+            // Fallback response in case both cache and network fail
+            return caches.match('/index.html');
+        })
+    );
+});
